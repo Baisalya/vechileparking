5a737fed9590527da9857a17202b2595343a05ff
Baisalya <baishalya1999@gmail.com>
2022-08-02 21:12:48 +0530

    Signup ui

--- src/main/java/com/lala/example/vechileparking/dbconnection.java
+++ src/main/java/com/lala/example/vechileparking/dbconnection.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 public class dbconnection {
    public Connection con;
    public Connection getCon(){
-       String dbname="";
-       String user="";
+       String dbname="parkingmanagment ";
+       String user="root";
        String password="";
-       String url="";
+       String url="jdbc:mysql://localhost:3306/parkingmanagment";
        try {
            Class.forName("com.mysql.jdbc.Driver");
-           con= DriverManager.getConnection(url,user,password);
+           con= DriverManager.getConnection(url,dbname,password);
        }catch (Exception e){
            e.printStackTrace();
 
